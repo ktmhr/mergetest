083418a62176be737f3b619c83baad5d8b84cdec
ktmhr <hifumi123@yahoo.co.jp>
2012-04-19 17:06:30 +0900

    C001

---
+++ app/Model/Version.php
@@ -0,0 +1,14 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Version Model
+ *
+ */
+class Version extends AppModel {
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+}

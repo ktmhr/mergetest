dff5fb5efcac76ee7babaf8a33001212d8a32582
ktmhr <hifumi123@yahoo.co.jp>
2012-04-19 17:08:35 +0900

    C002

---
+++ app/Controller/VersionsController.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Versions Controller
+ *
+ */
+class VersionsController extends AppController {
+
+/**
+ * Scaffold
+ *
+ * @var mixed
+ */
+	public $scaffold;
+
+}

163c945e366bb876c41b74d9616fc113d12dcd39
ktmhr <hifumi123@yahoo.co.jp>
2012-04-19 18:15:43 +0900

    B004

---
+++ app/Model/UsersFeed.php
@@ -0,0 +1,14 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * UsersFeed Model
+ *
+ */
+class UsersFeed extends AppModel {
+/**
+ * Primary key field
+ *
+ * @var string
+ */
+	public $primaryKey = 'deleted';
+}

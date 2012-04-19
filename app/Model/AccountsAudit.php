c69a837b6c6fc5420c12171c8863864333e0913c
ktmhr <hifumi123@yahoo.co.jp>
2012-04-19 16:51:00 +0900

    003

---
+++ app/Model/AccountsAudit.php
@@ -0,0 +1,14 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * AccountsAudit Model
+ *
+ */
+class AccountsAudit extends AppModel {
+/**
+ * Use table
+ *
+ * @var mixed False or table name
+ */
+	public $useTable = 'accounts_audit';
+}

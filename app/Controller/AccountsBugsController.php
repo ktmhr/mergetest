f007bb05ec0a0376138d84475c1e45743c972223
ktmhr <hifumi123@yahoo.co.jp>
2012-04-19 17:00:57 +0900

    B002

---
+++ app/Controller/AccountsBugsController.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * AccountsBugs Controller
+ *
+ */
+class AccountsBugsController extends AppController {
+
+/**
+ * Scaffold
+ *
+ * @var mixed
+ */
+	public $scaffold;
+
+}
